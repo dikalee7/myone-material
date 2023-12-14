7b870c01137151d9e31456f74a53017ccc503567
dikalee7 <dikalee7@gmail.com>
2023-12-15 08:12:37 +0900

    헤더 수정

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Container } from '@mui/material';
 import AccountBalanceWalletTwoToneIcon from '@mui/icons-material/AccountBalanceWalletTwoTone';
 import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
 import HomeIcon from '@mui/icons-material/Home';
@@ -11,6 +11,15 @@ import { useNavigate } from 'react-router-dom';
 const theme = createTheme({
   palette: {
     primary: teal,
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
   },
 });
 
@@ -33,52 +42,56 @@ const Header = () => {
     navigate('/');
   };
 
-  const homeArea = (
-    <HomeIcon
-      sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
-      onClick={goHome}
-    />
-  );
+  const homeArea = <HomeIcon sx={{ mr: 1 }} onClick={goHome} />;
 
   return (
     <>
       <ThemeProvider theme={theme}>
-        <AppBar position='static' style={{ padding: '1px' }}>
-          <Toolbar>
-            {headerInfo.hideBack ? (
-              <AccountBalanceWalletTwoToneIcon
-                sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
-              />
-            ) : (
-              <ArrowCircleLeftIcon
-                sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}
-                onClick={goBack}
-              />
-            )}
+        <AppBar position='static'>
+          <Container maxWidth='xl'>
+            <Toolbar disableGutters>
+              {headerInfo.hideBack ? (
+                <AccountBalanceWalletTwoToneIcon sx={{ mr: 1 }} />
+              ) : (
+                <ArrowCircleLeftIcon sx={{ mr: 1 }} onClick={goBack} />
+              )}
 
-            <Typography
-              variant='h6'
-              noWrap
-              // component='a'
-              // href='#app-bar-with-responsive-menu'
-              sx={{
-                mr: 2,
-                display: { xs: 'none', md: 'flex' },
-                fontWeight: 700,
-                letterSpacing: '.3rem',
-                color: 'inherit',
-                textDecoration: 'none',
-              }}
-            >
-              {headerInfo.title}
-            </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }} />
-            {!headerInfo.hideHome ? (
-              <Box sx={{ flexGrow: 0 }}>{homeArea}</Box>
-            ) : (
-              <Box sx={{ flexGrow: 0 }} />
-            )}
-          </Toolbar>
+              <Typography
+                variant='h6'
+                noWrap
+                // component='a'
+                // href='#app-bar-with-responsive-menu'
+                sx={{
+                  mr: 2,
+                  fontWeight: 700,
+                  letterSpacing: '.3rem',
+                  color: 'inherit',
+                  textDecoration: 'none',
+                }}
+              >
+                {headerInfo.title}
+              </Typography>
+              <Box sx={{ flexGrow: 1 }} />
+              {!headerInfo.hideHome ? (
+                <Box
+                  sx={{
+                    flexGrow: 0,
+                    // display: {
+                    //   xs: 'flex or none',
+                    //   sm: 'flex or none',
+                    //   md: 'flex or none',
+                    //   lg: 'flex or none',
+                    //   xl: 'flex or none',
+                    // },
+                  }}
+                >
+                  {homeArea}
+                </Box>
+              ) : (
+                <Box sx={{ flexGrow: 0 }} />
+              )}
+            </Toolbar>
+          </Container>
         </AppBar>
       </ThemeProvider>
     </>
